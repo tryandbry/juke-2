07b75c718720631e8c1bee25cd644c857b439249
Bryan Kao <bryan.w.kao@gmail.com>
2017-05-16 17:12:56 -0500

    completed 4.2

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import {Router, Route, hashHistory} from 'react-router';
+import Albums from './components/Albums';
+import {Router, Route, hashHistory, IndexRedirect} from 'react-router';
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={AppContainer} />
+    <Route path="/" component={AppContainer}>
+      <IndexRedirect to='albums' />
+      <Route path="albums" component={Albums} />
+    </Route>
   </Router>,
   document.getElementById('app')
 );
