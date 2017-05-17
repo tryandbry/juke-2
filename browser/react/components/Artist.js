f566f9a1fb7a954637e1de4cd90a224dde0a3a2b
Bryan Kao <bryan.w.kao@gmail.com>
2017-05-17 11:54:19 -0500

    completed section 7

--- browser/react/components/Artist.js
+++ browser/react/components/Artist.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import {Link} from 'react-router';
 import Songs from './Songs';
 import Albums from './Albums';
 
 class Artist extends React.Component {
-    constructor () {
-        super();
-    }
-    
-    componentDidMount () {
-        this.props.selectArtist(this.props.routeParams.artistId);
+  constructor() {
+    super();
+  }
+
+  componentDidMount(){
+    this.props.selectArtist(this.props.routeParams.artistId);
+  }
+
+  render () {
+    const selectedArtist = this.props.selectedArtist;
+    const children = this.props.children;
+    const propsToPassToChildren = {
+      songs:selectedArtist.songs,
+      albums:selectedArtist.albums || [],
+      currentSong:this.props.currentSong,
+      isPlaying:this.props.isPlaying,
+      toggleOne:this.props.toggle
     }
 
-    
-    render () { 
-        const selectedArtist = this.props.selectedArtist;
-        console.log(selectedArtist)
-        return (
-            <div>
-                <h3>{(selectedArtist.artist) ? selectedArtist.artist.name : ''}</h3>
-                <h4>ALBUMS</h4>
-                <Albums 
-                    albums={selectedArtist.albums || []}
-                />
-                <h4>SONGS</h4>
-                <Songs
-                    songs={selectedArtist.songs}
-                    currentSong={this.props.currentSong}
-                    isPlaying={this.props.isPlaying}
-                    toggleOne={this.props.toggle}
-                />
-            </div>
-        )
-    }
+    return (
+      <div>
+	<h3>{ selectedArtist.name }</h3>
+	<ul className="nav nav-tabs">
+	  <li><Link to={`/artists/${this.props.params.artistId}/albums`}>ALBUMS</Link></li>
+	  <li><Link to={`/artists/${this.props.params.artistId}/songs`}>SONGS</Link></li>
+	</ul>
+	{ children && React.cloneElement(children, propsToPassToChildren) }
+      </div>
+    )
+  }
 }
-
 
 export default Artist;
--- browser/react/index.js
+++ browser/react/index.js
@@ -5,6 +5,7 @@ import Albums from './components/Albums';
 import Album from './components/Album';
 import Artists from './components/Artists';
 import Artist from './components/Artist';
+import Songs from './components/Songs';
 
 import {Router, Route, hashHistory, IndexRedirect} from 'react-router';
 
@@ -15,7 +16,11 @@ ReactDOM.render(
       <Route path="albums" component={Albums} />
       <Route path="albums/:albumId" component={Album} />
       <Route path="artists" component={Artists} />
-      <Route path="artists/:artistId" component={Artist} />
+      <Route path="artists/:artistId" component={Artist} >
+	<IndexRedirect to='albums' />
+	<Route path="albums" component={Albums} />
+	<Route path="songs" component={Songs} />
+      </Route>
     </Route>
   </Router>,
   document.getElementById('app')
